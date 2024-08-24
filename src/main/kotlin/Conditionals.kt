37475eaa56a581329aa0332b6cf6a9c108274299
TanishqSehgal7 <tsgl7246@gmail.com>
2024-08-25 00:08:51 +0530

    Conditionals in Kotlin

--- src/main/kotlin/Conditionals.kt
+++ src/main/kotlin/Conditionals.kt
@@ -13,4 +13,50 @@ fun main() {
     }
     println("Value of z is $z")
 
+    val userInput = readLine() // taking user input in kotlin
+    println("You entered $userInput")
+
+    println(userInput?.uppercase()) // ? checks if userInput is null or not
+
+    if (userInput!=null)
+        println(userInput.toInt() - 5)
+
+    val age = readLine()
+
+    if (age != null) {
+        if(age < 18.toString()) {
+            println("You're not an adult yet!")
+        } else if (age > 18.toString() && age<=65.toString()) {
+            println("You're an adult")
+        } else {
+            println("You're really really old!")
+        }
+    }
+
+    // check if string is palindrome
+    val str = "racecar"
+    if (isPalindrome(str)) {
+        println("$str is a Palindromic String")
+    } else {
+        println("$str is not a Palindromic String")
+    }
+}
+
+fun isPalindrome(str:String) : Boolean {
+
+    var start = 0
+    var end = str.length-1
+    var isPalindrome = false;
+
+    while (start!=end) {
+        if(str.get(start) == str.get(end)) {
+            start++;
+            end --;
+            isPalindrome = true
+        } else {
+            isPalindrome = false
+        }
+    }
+
+    return isPalindrome
 }
