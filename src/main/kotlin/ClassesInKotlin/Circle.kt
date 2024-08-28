8b6de8de5f213c20b184133c69e30183f4937e18
TanishqSehgal7 <tsgl7246@gmail.com>
2024-08-28 23:46:24 +0530

    Visibility Modifiers

--- src/main/kotlin/ClassesInKotlin/Circle.kt
+++ src/main/kotlin/ClassesInKotlin/Circle.kt
@@ -1,9 +1,10 @@
 package ClassesInKotlin
 
-class Circle(val radius:Double) : Shape("Circle") {
+class Circle constructor(val radius:Double) : Shape("Circle") {
 
-    val PI: Double = 3.15
+//    the default visibility modifier in kotlin is public
 
+    private val PI: Double = 3.15 // only accessible to Circle class since it is private
     init {
         println("A circle is created with radius = $radius")
     }
@@ -11,4 +12,17 @@ class Circle(val radius:Double) : Shape("Circle") {
     fun calculateArea() = PI * radius * radius
 
     fun calculateCircumference() = 2 * PI * radius
+
 }
+
+/*
+
+We can also make our constructor private in the following way
+This means that we will only be able to instantiate objects of
+circle class inside circle class only which would be a rare case
+as far as real world usage is concerned
+
+class Circle private constructor(val radius:Double): Shape("Circle") {
+
+}
+*/
--- src/main/kotlin/ClassesInKotlin/Shape.kt
+++ src/main/kotlin/ClassesInKotlin/Shape.kt
@@ -1,10 +1,24 @@
 package ClassesInKotlin
 
-open class Shape(var name:String) {
+open class Shape(
+    var name:String) {
+
+    /* name is protected which means that name is accessible
+    to class Shape and all the class which inherit from the Shape class */
 
     init {
         println("I am the super class!")
     }
+
+//    protected fun changeName(newName:String) {
+//        name = newName
+//    }
+
+    /*
+    If changeName function is made protected, then it can be accessed
+    by the subclasses of Shape class in different or same package
+    but not by any other class
+    */
 
     fun changeName(newName:String) {
         name = newName
@@ -15,3 +29,4 @@ open class Shape(var name:String) {
 Open class is a class which can be a parent class for other classes
 ie, other classes can inherit from the class which is declared as open
 */
+
